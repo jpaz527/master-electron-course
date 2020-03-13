8e6d4d0a3a5500e244a65b2e8959e7bbd5dc1052
jpaz <jorge.paz@laureate.net>
2020-03-13 10:36:37 -0600

    Added several event handlers for the app.

--- main.js
+++ main.js
@@ -1,49 +1,60 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
-const windowStateKeeper = require('electron-window-state');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const windowStateKeeper = require("electron-window-state");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
+
+ipcMain.on('new-item', (e, itemUrl) => {
+  setTimeout(() => {
+    e.sender.send('new-item-success', 'New item from main process');
+  }, 2000)
+  
+});
 
 // Create a new BrowserWindow when `app` is ready
-function createWindow () {
-
+function createWindow() {
   // Win state keeper
   let state = windowStateKeeper({
-    defaultWidth: 500, defaultHeight: 650
+    defaultWidth: 500,
+    defaultHeight: 650
   });
 
   mainWindow = new BrowserWindow({
-    x: state.x, y: state.y,
-    width: state.width, height: state.height,
-    minWidth: 350, maxWidth: 650, minHeight: 300, 
+    x: state.x,
+    y: state.y,
+    width: state.width,
+    height: state.height,
+    minWidth: 350,
+    maxWidth: 650,
+    minHeight: 300,
     webPreferences: { nodeIntegration: true }
   });
 
   // Load index.html into the new BrowserWindow
-  mainWindow.loadFile('renderer/main.html')
+  mainWindow.loadFile("renderer/main.html");
 
   state.manage(mainWindow);
 
   // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Listen for window being closed
-  mainWindow.on('closed',  () => {
-    mainWindow = null
-  })
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
 
 // Electron `app` is ready
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed - (Not macOS - Darwin)
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
 
 // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
-app.on('activate', () => {
-  if (mainWindow === null) createWindow()
-})
+app.on("activate", () => {
+  if (mainWindow === null) createWindow();
+});
--- renderer/app.js
+++ renderer/app.js
@@ -1,3 +1,40 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+// Modules
+const { ipcRenderer } = require("electron");
+
+// Dom Nodes
+let showModal = document.querySelector("#show-modal");
+let closeModal = document.querySelector("#close-modal");
+let modal = document.querySelector("#modal");
+let addItem = document.querySelector("#add-item");
+let itemUrl = document.querySelector("#url");
+
+// Show modal
+showModal.addEventListener("click", e => {
+  modal.style.display = "flex";
+  itemUrl.focus();
+});
+// Hide Modal
+closeModal.addEventListener("click", e => {
+  modal.style.display = "none";
+});
+// Handle new item
+addItem.addEventListener("click", e => {
+  // Check a URL exists
+  if (itemUrl.value) {
+    // Send new item URL to main process
+    console.log(`Item URL: ${itemUrl.value}`);
+    
+    ipcRenderer.send('new-item', itemUrl.value);
+  }
+});
+
+// Listen for keyboard submit
+itemUrl.addEventListener("keyup", e => {
+  if (e.key === "Enter") addItem.click();
+});
+
+// Listen for new item from main process
+ipcRenderer.on('new-item-success', (e, newItem) => {
+  console.log(newItem);
+  
+})
