336340b2fcaa8cee9d6a304b81c03c35a77e4fcb
jpaz <jorge.paz@laureate.net>
2020-03-13 11:06:25 -0600

    Added main functionality for grabbing thumbnail of webpage using offscreen rendering.

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 // Modules
 const { app, BrowserWindow, ipcMain } = require("electron");
 const windowStateKeeper = require("electron-window-state");
+const readItem = require('./readItem');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-ipcMain.on('new-item', (e, itemUrl) => {
-  setTimeout(() => {
-    e.sender.send('new-item-success', 'New item from main process');
-  }, 2000)
-  
+ipcMain.on("new-item", (e, itemUrl) => {
+  readItem(itemUrl, item => {
+    e.sender.send('new-item-success', item);
+  });
 });
 
 // Create a new BrowserWindow when `app` is ready
